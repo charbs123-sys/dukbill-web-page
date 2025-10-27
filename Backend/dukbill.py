52e7b000a5d9ff0ad50bbb9c48c3327c01b26e77
Charbel Badr <158476419+charbs123-sys@users.noreply.github.com>
2025-10-27 16:04:30 +1100

    Update dukbill.py

--- Backend/dukbill.py
+++ Backend/dukbill.py
@@ -313,6 +313,8 @@ async def get_client_dashboard_broker(client_id: int, user=Depends(get_current_u
     claims, _ = user
     client = verify_client(client_id)
     client_user = get_user_from_client(client_id)
+    print("this is client user")
+    print(client_user)
     emails = get_client_emails(client_user["client_id"])
     headings = get_client_dashboard(client_id, emails)
     print(emails)
